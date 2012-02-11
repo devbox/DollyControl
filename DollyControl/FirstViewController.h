9d51928858b301bc41b73e7abba2592387d50234
David Rentsch <david.rentsch@me.com>
2012-02-11 15:54:11 +0100

    Initial Commit

---
+++ DollyControl/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  DollyControl
+//
+//  Created by David Rentsch on 11.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ DollyControl/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  DollyControl
+//
+//  Created by David Rentsch on 11.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
